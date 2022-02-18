a87e95dcb85505a5203afaba9bef907fb50df68e
Matt Umut Ucak <umutucak79@gmail.com>
2022-02-18 18:31:03 +0100

    children out of bounds error fixed, but generation is not correct/consistent

--- src/Agent.py
+++ src/Agent.py
@@ -1,15 +1,16 @@
 from ast import If
 from queue import Queue
+from turtle import back
 
 class Agent:
 	def __init__(self, graph, spawn_point, destination_point):
 		self.graph = graph
-		self.position = (spawn_point[0], spawn_point[1])
+		self.spawn_point = (spawn_point[0], spawn_point[1])
 		self.destination = (destination_point[0], destination_point[1])
 
 
-	def get_position(self):
-		return self.position
+	def get_spawn_point(self):
+		return self.spawn_point
 
 	def get_destination(self):
 		return self.destination
@@ -19,8 +20,9 @@ class Agent:
 		backtracking_table = {}
 		visited = []
 		found = False
-		queue.put(self.position)
-		visited.append(self.position)
+
+		queue.put(self.spawn_point)
+		visited.append(self.spawn_point)
 
 		while not found:
 			current_cell = queue.get()
@@ -31,31 +33,36 @@ class Agent:
 				children = self.generate_children(current_cell, visited)
 
 				for child in children:
-					if child not in visited:
-						visited.append(child)
-						queue.put(child)
-						backtracking_table[child] = current_cell
+					visited.append(child)
+					queue.put(child)
+					backtracking_table[child] = current_cell
 
 		return self.back_track(current_cell, backtracking_table)
 
 	def generate_children(self, current_cell, visited):
 		children = []
 
-		if self.graph[current_cell[0] + 1][current_cell[1]] == 1 and (current_cell[0]+1, current_cell[1]) not in visited:
-			children.append((current_cell[0] + 1, current_cell[1])) #going right
-		if self.graph[current_cell[0]][current_cell[1] + 1] == 1 and (current_cell[0], current_cell[1] + 1) not in visited:
-			children.append((current_cell[0], current_cell[1] + 1)) #going up
-		if self.graph[current_cell[0]][current_cell[1] - 1] == 1 and (current_cell[0], current_cell[1] - 1) not in visited:
-			children.append((current_cell[0], current_cell[1] - 1)) #going down
-		if self.graph[current_cell[0] - 1][current_cell[1]] == 1 and (current_cell[0] - 1, current_cell[1]) not in visited:
-			children.append((current_cell[0] - 1, current_cell[1])) #going left
+		# the outer ifs are checking if we are on the borders, the inner ifs are checking if the neighbors are 1s
+		if current_cell[0] != self.graph.width-1: #going right
+			if self.graph.array[current_cell[0] + 1][current_cell[1]] == 1 and (current_cell[0] + 1, current_cell[1]) not in visited:
+				children.append((current_cell[0] + 1, current_cell[1]))
+		if current_cell[0] != 0: #going left
+			if self.graph.array[current_cell[0] - 1][current_cell[1]] == 1 and (current_cell[0] - 1, current_cell[1]) not in visited:
+				children.append((current_cell[0] - 1, current_cell[1]))
+		if current_cell[1] != self.graph.width-1: #going down
+			if self.graph.array[current_cell[0]][current_cell[1] + 1] == 1 and (current_cell[0], current_cell[1] + 1) not in visited:
+				children.append((current_cell[0], current_cell[1] + 1))
+		if current_cell[1] != 0: #going up
+			if self.graph.array[current_cell[0]][current_cell[1] - 1] == 1 and (current_cell[0], current_cell[1] - 1) not in visited:
+				children.append((current_cell[0], current_cell[1] - 1))
 
 		return children
 
 	def back_track(self, current_cell, backtracking_table):
 		path = []
 
-		while current_cell != self.position:
+		while current_cell != self.spawn_point:
+			print(current_cell, backtracking_table[current_cell])
 			path.append(backtracking_table[current_cell])
 			current_cell = backtracking_table[current_cell]
 
--- src/Main.py
+++ src/Main.py
@@ -10,6 +10,6 @@ spawn_point = graph.get_spawn_point()
 destination = graph.get_destination(spawn_point[0])
 
 agent = Agent.Agent(graph, spawn_point, destination)
-print(agent.get_position(), agent.get_destination())
+print(agent.get_spawn_point(), agent.get_destination())
 print('---------')
 print(agent.find_destination_BFS())
